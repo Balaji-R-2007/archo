f6c23299b04596f2ec4011cf5dc4ee8fba948977
Harry Whittington <harrydpw@gmail.com>
2025-04-24 19:35:02 +0100

    redoing the homepage UI using motion library

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,23 +1,44 @@
-
 import React from 'react';
+import * as motion from "motion/react-client"
 
 const StartScreen = ({ onStartGame }) => {
   return (
-    <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-70 text-white">
-      <h1 className="text-6xl mb-6 font-bold">ARCHO</h1>
-      <div className="mb-8 text-center max-w-md">
-        <p className="mb-4">Use WASD or arrow keys to control your ship.</p>
-        <p className="mb-4">Spacebar to shoot. Destroy all asteroids to advance to the next level.</p>
-        <p>Watch out! If your ship gets hit, you'll lose a life!</p>
-      </div>
-      <button
-        className="px-8 py-4 bg-blue-600 text-white text-xl rounded-lg hover:bg-blue-700 transition"
+    <div className="relative w-full h-screen bg-black text-yellow-400 font-pixel overflow-hidden p-10 flex flex-col items-center justify-center text-center">
+
+      <motion.h1
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1}}
+            className="text-cyan-400 text-7xl drop-shadow-[6px_0_6px_rgba(0,255,255,0.75)] mb-10"
+          >
+            ARCHO
+      </motion.h1>
+      
+      <motion.div
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          className="max-5xl text-xl drop-shadow-[6px_0_6px_rgba(255,238,88,0.75)]"
+        >
+          <p>USE WASD OR ARROW KEYS TO MOVE YOUR SHIP.</p>
+          <p>SPACEBAR TO SHOOT. DESTROY ALL ASTEROIDS TO ADVANCE TO THE NEXT LEVEL.</p>
+          <p>WATCH OUT! IF YOUR SHIP GETS HIT, YOU'LL LOSE A LIFE!</p>
+
+      </motion.div>
+
+      <motion.button
+        initial={{scale: 0}}
+        animate={{scale: 1}}
+        transition={{type: "spring"}}
         onClick={onStartGame}
+        className="z-10 mt-12 px-10 py-5 text-xl tracking-widest text-yellow-300 border-4 border-pink-800 bg-black hover:bg-pink-900 shadow-[0_0_25px_#ff00ff] transition-all animate-bounce"
       >
         START GAME
-      </button>
+
+      </motion.button>
     </div>
   );
 };
+
 
 export default StartScreen;
